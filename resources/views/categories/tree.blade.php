b471c3f8a69c4e072af2f83359c361ec4ec563ce
Annequirage <c.marchand95@aol.com>
2020-07-08 10:16:10 +0200

    debug

--- resources/views/categories/tree.blade.php
+++ resources/views/categories/tree.blade.php
@@ -1,12 +1,12 @@
 <ul>
     @foreach ($categories as $category)
-        @if ()
+        
             
         <li><input type="radio" name="category_id" value="{{$category->id}}" @if ($category->id==$current) checked @endif> {{$category->name}} 
             
             @include('categories.tree',['categories'=> $category->children, "current"=>$current])
             
-        </li> @endif
+        </li> 
         
     @endforeach
   </ul>
